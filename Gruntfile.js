9baa7283c2b368546b72f848f2156fd4ace4e789
Francis Brito <fr.br94@gmail.com>
2014-01-09 22:49:05 -0400

    Added testing framework setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,21 @@
 'use strict';
 
 module.exports = exports = function (grunt) {
-    grunt.initConfig({});
+    grunt.initConfig({
+        mochaTest: {
+            structures: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: [
+                    'tests/structures/*.js'
+                ]
+            }
+        }
+    });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('test', ['mochaTest:structures']);
+    grunt.registerTask('default', ['test']);
 };
