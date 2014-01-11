a408bdaed144b5500ba76bf3583d106f329c67c3
Francis Brito <fr.br94@gmail.com>
2014-01-10 22:34:05 -0400

    Added tests for missing DynamicArray methods.

--- tests/structures/dynamic_array.js
+++ tests/structures/dynamic_array.js
@@ -10,6 +10,7 @@ describe('DynamicArray', function () {
 
             array.size.should.equal(0);
         });
+        // TODO: Consider rewriting these tests so the belong to #pop/#push as the depend on those.
         it('should increase by 1 everytime an item is pushed.', function () {
             var array = new DynamicArray();
 
@@ -35,5 +36,21 @@ describe('DynamicArray', function () {
             array.pop();
             array.size.should.equal(0);
         });
+    });
+    describe('#at', function () {
+        it('should retrieve the element given its index.');
+        it('should throw if index is out of bounds.');
+    });
+    describe('#push', function () {
+        it('should add an element to the collection.');
+    });
+    describe('#pop', function () {
+        it('should throw if array size is 0.');
+        it('should remove an element from the collection.');
+        it('should return the last element of the collection.');
+    });
+    describe('#indexOf', function () {
+        it('should return an index given an element.');
+        it('should return -1 if element is not in the collection.');
     });
 });
