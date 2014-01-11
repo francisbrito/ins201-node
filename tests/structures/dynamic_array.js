7be59a7bfda550f32758d1ca4f6f529f24e769ee
Francis Brito <fr.br94@gmail.com>
2014-01-10 21:59:17 -0400

    Added tests for DynamicArray#size property.

--- tests/structures/dynamic_array.js
+++ tests/structures/dynamic_array.js
@@ -10,5 +10,30 @@ describe('DynamicArray', function () {
 
             array.size.should.equal(0);
         });
+        it('should increase by 1 everytime an item is pushed.', function () {
+            var array = new DynamicArray();
+
+            array.push(1);
+            array.size.should.equal(1);
+            array.push(2);
+            array.size.should.equal(2);
+            array.push(3);
+            array.size.should.equal(3);
+        });
+        it('should decrease by 1 everytime an item is popped.', function () {
+            var array = new DynamicArray();
+
+            array.push(1);
+            array.push(2);
+            array.push(3);
+
+            array.size.should.equal(3);
+            array.pop();
+            array.size.should.equal(2);
+            array.pop();
+            array.size.should.equal(1);
+            array.pop();
+            array.size.should.equal(0);
+        });
     });
 });
